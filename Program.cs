225f22411eaf177db990b713c3fdfb7b3bc85c26
root <root@ip-172-31-46-185.ap-south-2.compute.internal>
2024-11-08 08:44:07 +0000

    my-dotnet-application

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace hello_world_api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                // Bind to all network interfaces (0.0.0.0) and listen on port 5000
+                .UseUrls("http://0.0.0.0:5000")  
+                .UseStartup<Startup>()  // Specify the Startup class
+                .Build();
+    }
+}
